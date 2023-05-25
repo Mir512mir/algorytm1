5606e3df707c703bb95b4499fb08ead674b7fe95
Mir512mir <123588015+Mir512mir@users.noreply.github.com>
2023-05-25 23:01:26 +0200

    Add files via upload

---
+++ Nowy folder/script.js
@@ -0,0 +1,11 @@
+function add(a,b,c) {
+    return a+b+c;
+    
+}
+let number1=parseFloat(prompt('Wwedite 1 czislo:'));
+let number2=parseFloat(prompt('Wwedite 2 czislo:'));
+let number3=parseFloat(prompt('Wwedite 3 czislo:'));
+
+let result=add(number1,number2,number3);
+
+console.log('Suma:' + result);
